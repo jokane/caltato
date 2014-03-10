667d76dddb0f3fad9c39885553513db0ba83e4bc
Jason O'Kane <jokane@cse.sc.edu>
2014-03-10 14:46:09 -0400

    Refactored syntax for more flexibility later.

--- task.py
+++ task.py
@@ -69,7 +69,7 @@ def main(args):
   (calService, taskService) = getServices()
 
   # When are we now?
-  now = datetime.datetime.today()
+  today = datetime.date.today()
   
   # Get the calendar events.
   token = None
@@ -84,42 +84,54 @@ def main(args):
     for event in events['items']:
       summary = event.get('summary', '').encode('ascii', 'ignore')
       description = event.get('description', '').encode('ascii', 'ignore')
-      for match in re.finditer(r"task\((.*)\)", description):
-        # Remember the command we just found.
-        command = match.group(1) 
+      for line in description.split("\n"):
+        for match in re.finditer(r"^task:(.*)", line):
+          # Remember the command we just found.
+          command = match.group(1) 
+          print "Command: ", command
 
-        # Split it into an offset and a message.
-        match = re.match(r"([+-]\d*), \"?(.*)\"?", command)
-        if match is not None:
-          interval = int(match.group(1))
-          message = match.group(2)
-        else:
-          fail("Could not parse this command:" + command)
+          # Look for special codes on this line:
+          # 1. Extract an offset from the event's date, if any.
+          offset = 0
+          offsetPattern = r"^\s*([+-]?\d+)"
+          match = re.match(offsetPattern, command)
+          if match is not None:
+            offset = int(match.group(1))
+            command = re.sub(offsetPattern, "", command, 1)
 
-        # The results can contain either a date or a datetime, depending on
-        # whether or not it's an all-day event.  These different kinds of
-        # answers appear in differnent places in the 'start' hash.  Since we
-        # only want the date part, extract just that.
-        match = re.search(
-          r"(\d\d\d\d)-(\d\d)-(\d\d)",
-          event['start'].__repr__()
-        )
-        eventStart = datetime.date(
-          int(match.group(1)),
-          int(match.group(2)),
-          int(match.group(3))
-        )
+          # 2. ....that's it for now.
 
-        # Figure out when this task should be added to the task list.
-        target = eventStart + datetime.timedelta(days=interval)
+          # Everything left over is the message.  Clean it up a little.
+          message = command
+          message = re.sub("^ *", "", message)
+          message = re.sub(" +", " ", message)
 
-        print "Summary: ", summary
-        print "Command: ", command
-        print "Event start: ", eventStart
-        print "Target date: ", target
-        print "Interval: ", interval
-        print "Message: ", message
-        print
+          # The results can contain either a date or a datetime, depending on
+          # whether or not it's an all-day event.  These different kinds of
+          # answers appear in differnent places in the 'start' hash.  Since we
+          # only want the date part, extract just that.
+          match = re.search(
+            r"(\d\d\d\d)-(\d\d)-(\d\d)",
+            event['start'].__repr__()
+          )
+          eventStart = datetime.date(
+            int(match.group(1)),
+            int(match.group(2)),
+            int(match.group(3))
+          )
+
+          # Figure out when this task should be added to the task list.
+          target = eventStart + datetime.timedelta(days=offset)
+
+          # Is today the target date?
+          print "Summary: ", summary
+          print "Event start: ", eventStart
+          print "Target date: ", target
+          print "Offset: ", offset
+          print "Message: ", message
+          if target == today:
+            print "***TODAY***"
+          print 
 
     token = events.get('nextPageToken')
     if token is None:
